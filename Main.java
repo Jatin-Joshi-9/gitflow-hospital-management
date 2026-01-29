11b7ac6cf934d03d1d2323b7721f9200b849986b
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-01-29 07:37:38 +0530

    relese :release v1.0.0 of hospital management system

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hospital Management System");
+        System.out.println("Hospital Management System v1.0.0");
         Patient patient = new Patient("John Doe", 30);
         System.out.println("Patient Name: " + patient.getName());
         System.out.println("Patient Age: " + patient.getAge()); 
