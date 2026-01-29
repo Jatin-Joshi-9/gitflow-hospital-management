dcea0f7e08e76322b07e86fc98638b1b30fadedc
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-01-29 07:31:12 +0530

    feat: create Patient object in Main

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hospital Management System");
+        Patient patient = new Patient("John Doe", 30);
+        System.out.println("Patient Name: " + patient.getName());
+        System.out.println("Patient Age: " + patient.getAge()); 
     }
 }
